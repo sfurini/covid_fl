72489735401a3cad50dc6eed6ab1bd7fce2e7a92
sfurini <furini.simone@gmail.com>
2020-12-22 15:53:15 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import warnings
+warnings.filterwarnings("ignore")
+
 import sys
 import socket
 import time
@@ -39,28 +42,46 @@ server_ip = 'localhost'
 server_port = 10000
 
 #----- INITIALIZATION
-
+age_max = 100
 bim = pd.read_csv('./MergeM2.bim', header = None, sep = '\t')
 ped = pd.read_csv('./MergeM2.ped', header = None, index_col = 0, sep = '\t')
 ped.drop(ped.columns[range(5)], axis = 1, inplace = True)
 ref_ref = (bim[5] + ' ' + bim[5]).values.reshape((1,-1)) 
 feature_genes = pd.DataFrame((ped.values != ref_ref).astype(int), columns = bim[1], index = ped.index) # this dataframe has samples along the rows and genes along the columns, features_local[i,j] is 1 if gene j in sample i is mutated
 pheno = pd.read_csv('./pheno.txt', sep = ' ', index_col = 0)
-covar = pd.read_csv('./covar.txt', sep = ' ', index_col = 0)
-index_samples = set(feature_genes.index) & set(pheno.index) & set(covar.index)
+covar_unnorm = pd.read_csv('./covar.txt', sep = ' ', index_col = 0)
+index_samples = set(feature_genes.index) & set(pheno.index) & set(covar_unnorm.index)
 phenotypes = pheno.loc[index_samples]['A2'] # where A2 is the name of the column of the pheno.txt considered for the analysis
-age_max = 100
-covar = covar.loc[index_samples][['age', 'sex']]
+covar = covar_unnorm.loc[index_samples][['age','sex']]
 covar['age'][covar['age'] > age_max] = age_max
 covar['age'] = covar['age'] / age_max
 covar['age*sex'] = covar['age']*covar['sex']
 covar['age^2'] = covar['age']**2
 covar['age^2*sex'] = covar['age']**2*covar['sex']
+pca_list = []
+for name in covar_unnorm.columns:
+    if 'PC' in name:
+        covar[name] = covar_unnorm[name] - np.min(covar_unnorm[name])  / (np.max(covar_unnorm[name]) - np.min(covar_unnorm[name]))
+        pca_list.append(name)
 X = np.concatenate((covar.values, feature_genes.loc[index_samples].values), axis = 1) # np.array of input features
 y = phenotypes.values # np.array of output classes
-feature_names = np.array(['age','sex','age*sex','age^2','age^2*sex'] + list(feature_genes.columns))
-print('Number of samples: {}'.format(X.shape[0]))
-print('Number of features: {}'.format(X.shape[1]), len(feature_names))
+feature_names = np.array(['age','sex','age*sex','age^2','age^2*sex'] + pca_list + list(feature_genes.columns))
+if len(sys.argv) == 3:
+    pca_df = pd.read_csv('rarePCA.txt.eigenvec', header = None, sep = ' ', index_col = 0)
+    pca_df.drop(pca_df.columns[0], axis = 1, inplace = True)
+    pca_df = pca_df.loc[index_samples]
+    pca1 = pca_df.values[:,0]
+    pca1_perc = np.digitize(pca1, np.percentile(pca1, np.linspace(0,100,int(sys.argv[2]) + 1)[1:-1]))
+    inds = pca1_perc == int(sys.argv[1])
+    #inds = np.random.choice(range(X.shape[0]), int(0.5*X.shape[0]), replace = False)
+    #if int(sys.argv[2]) == 0:
+    #    inds = range(int(0.5*X.shape[0]))
+    #else:
+    #    inds = range(int(0.5*X.shape[0])+1,X.shape[0])
+    X = X[inds,:]
+    y = y[inds]
+print('Number of samples: {} {}'.format(X.shape[0], len(y)))
+print('Number of features: {} {}'.format(X.shape[1], len(feature_names)))
 print('Samples per phenotype class: {}'.format(', '.join(['{} = {}'.format(class_id, n_samples) for class_id, n_samples in zip(*np.unique(y, return_counts = True))])))
 
 #--- dummy
@@ -76,7 +97,6 @@ print('Samples per phenotype class: {}'.format(', '.join(['{} = {}'.format(class
 #pred_23 = (np.logical_xor(x2,x3).astype(int) + noise_term) % 2
 #y = np.empty(n_samples)
 #if len(sys.argv) == 1: # combination of two rules
-#    print('# Combination of two rules')
 #    y[:n_samples_half] = pred_01[:n_samples_half]
 #    y[n_samples_half:] = pred_23[n_samples_half:]
 #    X = np.concatenate((x0.reshape(-1,1),x1.reshape(-1,1),x2.reshape(-1,1),x3.reshape(-1,1)), axis = 1)
@@ -86,9 +106,8 @@ print('Samples per phenotype class: {}'.format(', '.join(['{} = {}'.format(class
 #        noise_term =  np.random.randint(low = 0, high = 2, size = (n_samples,1))
 #        X = np.concatenate((X, noise_term), axis = 1)
 #    feature_names = ['A'+str(i) for i in range(X.shape[1])]
-#elif len(sys.argv) == 2: # using alternatively one or the other rule
+#elif len(sys.argv) > 1: # using alternatively one or the other rule
 #    if int(sys.argv[1]) % 2  == 0:
-#        print('# Using rule 0')
 #        y = pred_01
 #        X = np.concatenate((x0.reshape(-1,1),x1.reshape(-1,1),x2.reshape(-1,1),x3.reshape(-1,1)), axis = 1)
 #        poly = PolynomialFeatures(3, include_bias = False)
@@ -98,7 +117,6 @@ print('Samples per phenotype class: {}'.format(', '.join(['{} = {}'.format(class
 #            X = np.concatenate((X, noise_term), axis = 1)
 #        feature_names = ['A'+str(i) for i in range(50)] +  ['B'+str(i) for i in range(50, X.shape[1])]
 #    else:
-#        print('# Using rule 1')
 #        y = pred_23
 #        X = np.concatenate((x0.reshape(-1,1),x1.reshape(-1,1),x2.reshape(-1,1),x3.reshape(-1,1)), axis = 1)
 #        poly = PolynomialFeatures(3, include_bias = False)
@@ -110,6 +128,11 @@ print('Samples per phenotype class: {}'.format(', '.join(['{} = {}'.format(class
 #X, y = shuffle(X, y)
 #--- END: dummy
 
+if len(sys.argv) > 1:
+    fout = open('client_{}.out'.format(sys.argv[1]), 'wt')
+else:
+    fout = open('client.out','wt')
+i_iter = 0
 #----- END: INITIALIZATION
 
 soc = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
@@ -120,14 +143,17 @@ except BaseException as e:
     soc.close()
     print('Socket Closed')
 print('Successful Connection to the Server\n')
-
 while True:
-    print("Receiving from the Server")
+    #print("Receiving from the Server")
     received_message, status = recv(soc=soc, buffer_size = 4096, recv_timeout = 10)
     if status == 0:
         break
-    #print('DEBUG',received_message, end="\n\n")
-    response_message = {}
+    print('DEBUG>',received_message['what2do'],received_message['version'],received_message['i_iter'], end="\n\n")
+    if received_message['i_iter'] < i_iter:
+        fout.write('#END\n\n')
+        fout.flush()
+    i_iter = received_message['i_iter']
+    response_message = {'version':received_message['version']}
     if received_message['what2do'] == 'split':
         skf = StratifiedKFold(n_splits = received_message['k_folds'])
         train_indexes, test_indexes = [], []
@@ -137,11 +163,13 @@ while True:
         response_message['what2do'] = 'ready'
     elif received_message['what2do'] == 'train_all_samples':
         train_indexes, test_indexes = [range(X.shape[0]),], [range(X.shape[0]),] # these are not really used as training/testing set, it's just for consistency with the 'minimize' section in cross-validation steps
+        fout.write('#END\n\n')
+        fout.flush()
         response_message['what2do'] = 'ready'
     elif received_message['what2do'] == 'minimize':
         model = received_message['model']
         if isinstance(received_message['coef_'], pd.Series):
-            model.coef_ = received_message['coef_'].loc[feature_names].values
+            model.coef_ = received_message['coef_'].reindex(feature_names, fill_value = 0.0)
             model.intercept_ = np.array([model.intercept_,])
         i_fold = received_message['i_fold']
         if i_fold >= len(train_indexes):
@@ -149,12 +177,16 @@ while True:
         else:
             train_index = train_indexes[i_fold]
             test_index = test_indexes[i_fold]
-            model.partial_fit(X[train_index,:], y[train_index], classes = [0,1])
-            print('---logloss', log_loss(y[train_index], model.predict_proba(X[train_index,:]), eps=1e-15) )
-            print('---top features:',np.argsort(np.abs(model.coef_.flatten()))[-4::])
+            for j in range(1):
+                model.partial_fit(X[train_index,:], y[train_index], classes = [0,1])
+            cnf = confusion_matrix(y[test_index], model.predict(X[test_index,:]))
+            acc = np.sum(np.diag(cnf)) / np.sum(cnf)
+            loss = log_loss(y[train_index], model.predict_proba(X[train_index,:]), eps=1e-15)
+            loss_reg = loss + model.get_params()['alpha']*np.sum(np.abs(model.coef_))
+            fout.write('{}\t{}\t{}\t{}\t{}\t{}\n'.format(received_message['version'], loss, loss_reg, acc,model.get_params()['eta0'],np.argsort(np.abs(model.coef_.flatten()))[-4::]))
             response_message['what2do'] = 'update'
             response_message['n_samples'] = len(train_index)
-            response_message['confusion_matrix'] = confusion_matrix(y[test_index], model.predict(X[test_index,:]))
+            response_message['confusion_matrix'] = cnf 
             response_message['model'] = model
             response_message['coef_'] = pd.Series(model.coef_.flatten(), index = feature_names)
     elif received_message['what2do'] == 'echo':
@@ -162,16 +194,17 @@ while True:
     elif received_message['what2do'] == 'reset':
         response_message = received_message
     elif received_message['what2do'] == 'done':
+        fout.write('#END\n\n')
+        fout.flush()
         print('DONE')
         break
     else:
         print('Unrecognized message type')
         break
-
     response_json = json.dumps(response_message, cls = encdec.MessageEncoder) + '\4'
-    print('Sending Data to the Server')
+    #print('Sending Data to the Server')
     response_bin = bytes(response_json, encoding="utf-8")
     soc.sendall(response_bin)
-
 soc.close()
 print('Socket Closed\n')
+fout.close()
--- server.py
+++ server.py
@@ -11,11 +11,11 @@ import pandas as pd
 import functools
 print = functools.partial(print, flush=True)
 
-#import matplotlib.pyplot as plt
-#from matplotlib.backends.backend_pdf import PdfPages
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import SGDClassifier
+
+#import matplotlib.pyplot as plt
+#from matplotlib.backends.backend_pdf import PdfPages
 
 import encdec
 
@@ -39,9 +39,9 @@ class SocketThread(threading.Thread):
                 if data == b'':
                     received_data = b""
                     if (time.time() - self.recv_start_time) > self.recv_timeout:
-                        return None, 0 # 0 means the connection is no longer active and it should be closed.
+                        return None, 0 # 0 means the connection is no longer active and it should be closed
                 elif data.decode('utf-8')[-1] == '\4': # end of transmission
-                    #print("All data ({data_len} bytes) Received from {client_info}.".format(client_info=self.client_info, data_len=len(received_data)))
+                    #print('All data ({data_len} bytes) Received from {client_info}.'.format(client_info=self.client_info, data_len=len(received_data)))
                     if len(received_data) > 0:
                         try:
                             received_data = received_data.decode('utf-8')[:-1]
@@ -57,20 +57,23 @@ class SocketThread(threading.Thread):
                 return None, 0
 
     def reply(self, received_message):
-        global server_model, server_coef_, client_infos, confusion_matrix, acc_means, acc_stds, i_fold, k_folds, i_iter, i_reg, i_reg_best,  max_iter, regs
+        global server_model, server_coef_, client_infos, confusion_matrix, acc_means, acc_stds, i_fold, k_folds, i_iter, i_reg, i_reg_best,  max_iter, regs, server_model_version
         thread_lock.acquire()
         if (type(received_message) is dict):
-            response_message = {'what2do':'done'}
-            #print('DEBUG ',i_iter,i_fold,i_reg,received_message['what2do'])
-            if (i_iter == max_iter): # or any other condition to test the convergence
+            response_message = {'what2do':'done', 'version':server_model_version, 'i_iter': i_iter}
+            #print('DEBUG> ',i_iter,i_fold,i_reg,received_message['what2do'])
+            if (i_iter == max_iter):
                 for client_name, client_score in client_infos['scores'].items():
                     confusion_matrix[:,:,i_fold] += client_score
+                acc = np.sum(np.diag(confusion_matrix[:,:,i_fold])) / np.sum(confusion_matrix[:,:,i_fold])
+                print('fold {} Done --> accuracy = {}'.format(i_fold, acc))
                 client_infos['n_samples'] = {}
                 client_infos['scores']= {}
                 client_infos['coef_'] = {}
                 client_infos['models'] = {}
                 i_iter = 0
                 i_fold += 1
+                response_message['i_iter'] = i_iter
                 if i_fold == k_folds:
                     i_fold = 0
                     acc = np.array([np.sum(np.diag(confusion_matrix[:,:,i])) / np.sum(confusion_matrix[:,:,i]) for i in range(k_folds)])
@@ -86,11 +89,11 @@ class SocketThread(threading.Thread):
                         print('\taccuracy means:', acc_means)
                         print('\taccuracy stds:', acc_stds)
                         print('\toptimal regularization:', regs[i_reg_best])
-                        server_model = SGDClassifier(loss = 'log', penalty = 'l1', alpha = regs[i_reg_best], max_iter = 1, learning_rate = 'optimal', early_stopping = False, tol = None)
+                        server_model = SGDClassifier(loss = 'log', penalty = 'l1', alpha = regs[i_reg_best], max_iter = 1, learning_rate = 'constant', eta0 = learning_rate_0, early_stopping = False, tol = None)
                         server_coef_ = False
                         response_message['what2do'] = 'train_all_samples'
                     else:
-                        server_model = SGDClassifier(loss = 'log', penalty = 'l1', alpha = regs[i_reg], max_iter = 1, learning_rate = 'optimal', early_stopping = False, tol = None)
+                        server_model = SGDClassifier(loss = 'log', penalty = 'l1', alpha = regs[i_reg], max_iter = 1, learning_rate = 'constant', eta0 = learning_rate_0, early_stopping = False, tol = None)
                         server_coef_ = False
                         response_message['what2do'] = 'minimize'
                         response_message['model'] = server_model
@@ -106,8 +109,9 @@ class SocketThread(threading.Thread):
                         print('TOP FEATURES:',feature_names[ind_top])
                         print('TOP COEFFICIENTS:',coef_values[ind_top])
                         print('NUMBER OF FEATURES:',len(feature_names))
+                        server_coef_.to_csv('coef.csv')
                     else:
-                        server_model = SGDClassifier(loss = 'log', penalty = 'l1', alpha = regs[i_reg], max_iter = 1, learning_rate = 'optimal', early_stopping = False, tol = None)
+                        server_model = SGDClassifier(loss = 'log', penalty = 'l1', alpha = regs[i_reg], max_iter = 1, learning_rate = 'constant', eta0 = learning_rate_0, early_stopping = False, tol = None)
                         server_coef_ = False
                         response_message['what2do'] = 'minimize'
                         response_message['model'] = server_model
@@ -120,11 +124,18 @@ class SocketThread(threading.Thread):
                     response_message['coef_'] = server_coef_
                     response_message['i_fold'] = i_fold
                 elif received_message['what2do'] == 'update':
-                    client_infos['models'][self.client_info] = received_message['model']
-                    client_infos['coef_'][self.client_info] = received_message['coef_']
-                    client_infos['scores'][self.client_info] = received_message['confusion_matrix']
-                    client_infos['n_samples'][self.client_info] = received_message['n_samples']
-                    response_message['what2do'] = 'echo'
+                    if received_message['version'] == response_message['version']:
+                        client_infos['models'][self.client_info] = received_message['model']
+                        client_infos['coef_'][self.client_info] = received_message['coef_']
+                        client_infos['scores'][self.client_info] = received_message['confusion_matrix']
+                        client_infos['n_samples'][self.client_info] = received_message['n_samples']
+                        update_server_model(self.client_info)
+                        response_message['what2do'] = 'echo'
+                    else:
+                        response_message['what2do'] = 'minimize'
+                        response_message['model'] = server_model
+                        response_message['coef_'] = server_coef_
+                        response_message['i_fold'] = i_fold
                 elif received_message['what2do'] == 'reset':
                         response_message['what2do'] = 'split'
                         response_message['k_folds'] = k_folds
@@ -138,7 +149,6 @@ class SocketThread(threading.Thread):
                         response_message['i_fold'] = i_fold
                     else:
                         response_message['what2do'] = 'echo'
-                update_server_model()
             response_json = json.dumps(response_message, cls = encdec.MessageEncoder) + '\4'
             response_bin = bytes(response_json, encoding="utf-8")
             try:
@@ -151,7 +161,7 @@ class SocketThread(threading.Thread):
 
     def run(self):
         try:
-            message_json = json.dumps({'what2do':'reset'}, cls = encdec.MessageEncoder) + '\4'
+            message_json = json.dumps({'what2do':'reset','version':server_model_version,'i_iter':-1}, cls = encdec.MessageEncoder) + '\4'
             message_bin = bytes(message_json, encoding="utf-8")
             self.connection.sendall(message_bin)
         except BaseException as e:
@@ -168,42 +178,51 @@ class SocketThread(threading.Thread):
                 break
             self.reply(received_data)
 
-def update_server_model():
-    global server_model, server_coef_, client_infos, i_iter, i_fold, i_reg, min_updated_clients
+def update_server_model(client_info):
+    global server_model, server_coef_, client_infos, i_iter, i_fold, i_reg, min_updated_clients, server_model_version
+    print('I_ITER:',i_iter,'I_FOLD:',i_fold,'I_REG:',i_reg)
+    #print('Updating server model to {} using data from {}'.format(server_model_version,client_info))
+    server_model.intercept_ = client_infos['models'][client_info].intercept_
+    if isinstance(server_coef_, pd.Series):
+        server_coef_ = server_coef_.reindex(set(server_coef_.index) | set(client_infos['coef_'][client_info].index))
+        server_coef_[client_infos['coef_'][client_info].index] = client_infos['coef_'][client_info].values  
+    else:
+        server_coef_ = client_infos['coef_'][client_info]
     n_updates_from_clients = np.sum([n_samples > 0 for client_name, n_samples in client_infos['n_samples'].items()])
-    if n_updates_from_clients >= min_updated_clients:
-        n_total_samples = sum(client_infos['n_samples'].values())
-        coef_all = pd.DataFrame({client_name:client_infos['coef_'][client_name] for client_name, n_samples in client_infos['n_samples'].items() if n_samples > 0})
-        coef_all.fillna(0, inplace = True)
-        server_model.intercept_ = 0
+    if min_updated_clients == 1:
+        client_infos['n_samples'][client_info] = 0
+    elif n_updates_from_clients >= min_updated_clients:
         for client_name, n_samples in client_infos['n_samples'].items():
-            if n_samples > 0:
-                coef_all[client_name] *= n_samples
-                server_model.intercept_ +=  client_infos['models'][client_name].intercept_ * n_samples / n_total_samples
+            if (n_samples > 0) and (client_name != client_info):
                 client_infos['n_samples'][client_name] = 0
-        server_coef_ = coef_all.mean(axis = 1) / n_total_samples
-        print('I_ITER:',i_iter,'I_FOLD:',i_fold,'I_REG:',i_reg)
-        #print('SERVER MODEL:',server_model)
-        #print('CLIENT_INFOS:',client_infos)
-        i_iter += 1
+    server_model_version += 1
+    #server_model.eta0 = server_model.eta0 * (i_iter+1) / (i_iter + 2) # decreasing learning rate
+    i_iter += 1
 
 #----- INITIALIZATION
-regs = np.logspace(np.log10(1e-3), np.log10(1e-1), 10) # array with the tested values for regularization strengths
+regs = [0.02,]
+regs = [0.0129,]
+regs = [0.010772173450159416,]
+regs = [0.01,]
+regs = np.logspace(np.log10(1e-3), np.log10(1e-2), 10)[::-1] # array with the tested values for regularization strengths
+regs = [1e-2, ]
 k_folds = 10 # number of folds for cross-validation
-max_iter = 100 # maximum number of minimization steps
 i_reg = 0 # index in the array of regularization strenths
 i_fold = 0 # index of the fold for K-folds cross-validation
 i_iter = 0 # index of the minimization step
 i_reg_best = None # index of the optimal regularization strength
-min_updated_clients = 1 # the server model is updated only after receiving at least this number of updates from the clients
+learning_rate_0 = 1e-2 # starting learning rate
+min_updated_clients = 3 # the server model is updated only after receiving at least this number of updates from the clients
+max_iter = 100*min_updated_clients # maximum number of minimization steps
 confusion_matrix = np.zeros((2,2,k_folds)) # this one is used to store the confusion matrixes during K-folds cross-validation
 client_infos = {'n_samples':{}, 'scores':{}, 'models':{}, 'coef_':{}}
 acc_means = np.empty(len(regs)) # average accuracy over k-folds at different regularization strengths
 acc_stds = np.empty(len(regs)) # standard deviation of the accuracy over k-folds at different regularization strengths
-server_model = SGDClassifier(loss = 'log', penalty = 'l1', alpha = regs[i_reg], max_iter = 1, learning_rate = 'optimal', early_stopping = False, tol = None)
+server_model = SGDClassifier(loss = 'log', penalty = 'l1', alpha = regs[i_reg], max_iter = 1, learning_rate = 'constant', eta0 = learning_rate_0, early_stopping = False, tol = None)
 server_coef_ = False
 server_ip = 'localhost'
 server_port = 10000
+server_model_version = 0
 #----- END: INITIALIZATION
 
 soc = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
